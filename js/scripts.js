76348ef84e267bfe95330e007ea7b8b5ce32aeab
Reaper618 <113204129+Reaper618@users.noreply.github.com>
2022-11-08 22:49:43 -0600

    1.3

--- js/scripts.js
+++ js/scripts.js
@@ -22,13 +22,42 @@ function input(e) { //Revisara el valor en el input y si es menor de 4, entonces
 function entDep() {
     var str1 = document.getElementById("InputMonto").value;
     if (str1 > 0 && str1 < 1001) {
-        swal("Exito", "Se han depositado $" + str1, "success");
-        var cont = window.localStorage.getItem("saldo");
+swal("Se ha realizado el deposito, deseas generar un comprobante?", {
+  buttons: {
+    cancel: "No",
+    catch: {
+      text: "Si",
+      value: "true",
+    },
+  },
+})
+.then((value) => {
+  switch (value) {
+
+    case "true":
+		const { jsPDF } = window.jspdf; 
+
+	const doc = new jsPDF();
+		var fecha = new Date(); 
+		var nfecha = "Fecha de impresion : "+ fecha;
+		doc.text(10,50,nfecha);
+		doc.text("Has realizado un deposito de $" + str1, 10, 10); //mensaje que incluir el PDF
+		doc.save("a4.pdf");
+		  break;
+ 
+    default:
+      swal("Entendido","Puedes visitar la pagina de consulta de saldo si deseas ver tus transacciones luego","success");
+  }
+});         var cont = window.localStorage.getItem("saldo");
         cont = parseInt(cont, 10) + parseInt(str1, 10);
         window.localStorage.setItem("saldo", cont); //actualiza valor en localstorage con el valor del contador
-        var cont2 = window.localStorage.getItem("depositos");
+        var cont2 = window.localStorage.getItem("depositos");//suma 1 al numero de depositos que se ha hecho 
         cont2 = parseInt(cont2, 10) + 1;
-        window.localStorage.setItem("depositos", cont2); //suma 1 al numero de depositos que se ha hecho
+        window.localStorage.setItem("depositos", cont2); 
+			var tot=window.localStorage.getItem("totalIngresos");//lleva cuenta de total de ingresos para poder graficarlos
+		 tot = parseInt(tot,10) + parseInt(str1,10);
+		 window.localStorage.setItem("totalIngresos",tot);
+		
     }
     if (str1 > 1000) {
         swal("Error", "Este cajero acepta unicamente depositos de hasta $1000 por favor acuda a una sucursal para realizar su deposito", "info");
@@ -37,8 +66,6 @@ function entDep() {
     if (str1 == "") {
         swal("Error", "Ingresa un valor", "error");
     }
-    console.log(cont);
-
 }
 
 function input1(e) { //Revisara el valor en el input y si es menor de 5, entonces agregara al final el valor del boton presionado
@@ -57,13 +84,41 @@ function inputPred(e) { //Input de los valores predeterminados, limpia el monto 
 function entRet() {
     var str1 = document.getElementById("InputMonto").value;
     if (str1 > 0 && str1 < 1001) {
-        swal("Exito", "Se han retirado $" + str1, "success");
-        var cont = window.localStorage.getItem("saldo");
+swal("Se ha realizado el retiro, deseas generar un comprobante?", {
+  buttons: {
+    cancel: "No",
+    catch: {
+      text: "Si",
+      value: "true",
+    },
+  },
+})
+.then((value) => {
+  switch (value) {
+
+    case "true":
+		const { jsPDF } = window.jspdf; 
+
+	const doc = new jsPDF();
+		  var fecha = new Date(); 
+		var nfecha = "Fecha de impresion : "+ fecha;
+		doc.text(10,50,nfecha);
+		  doc.text("Has realizado un retiro de $" + str1, 10, 10); //mensaje que incluir el PDF
+		doc.save("a4.pdf");
+		  break;
+ 
+    default:
+      swal("Entendido","Puedes visitar la pagina de consulta de saldo si deseas ver tus transacciones luego","success");
+  }
+});        var cont = window.localStorage.getItem("saldo");
         cont = parseInt(cont, 10) - parseInt(str1, 10);
         window.localStorage.setItem("saldo", cont);
-        var cont2 = window.localStorage.getItem("retiros");
+        var cont2 = window.localStorage.getItem("retiros");//suma 1 al numero de retiros que se ha hecho
         cont2 = parseInt(cont2, 10) + 1;
-        window.localStorage.setItem("retiros", cont2); //suma 1 al numero de retiros que se ha hecho
+        window.localStorage.setItem("retiros", cont2); 
+			var tot=window.localStorage.getItem("totalRetiros");//lleva cuenta de total de retiros para poder graficarlos
+		 tot = parseInt(tot,10) + parseInt(str1,10);
+		 window.localStorage.setItem("totalRetiros",tot);
     }
     if (str1 > 1000) {
         swal("Error", "Este cajero acepta unicamente retiros de hasta $1000 por favor acuda a una sucursal para realizar su retiro", "info");
@@ -83,10 +138,15 @@ function checkSaldo() { //revisa si es localstorage ya tiene saldo guardado, si 
         window.localStorage.setItem("saldo", 500); //saldo inicial
         window.localStorage.setItem("depositos", 0);
         window.localStorage.setItem("retiros", 0);
-    }
+        window.localStorage.setItem("totalIngresos", 500); //saldo inicial
+        window.localStorage.setItem("totalRetiros", 0);
+
+	 }
     document.getElementById("saldo").innerHTML = localStorage.getItem("saldo"); //muestra el valor de localstorage en el menu principal
 }
 
+
+function loadNum(){ //carga tabla de n transacciones
 const labels = [
             'Depositos',
             'Retiros',
@@ -95,7 +155,7 @@ const labels = [
 const data = {
     labels: labels,
     datasets: [{
-        label: 'Retiros',
+        label: 'Transacciones',
         backgroundColor: 'rgb(255, 255, 132)',
         borderColor: 'rgb(255, 99, 132)',
         data: [window.localStorage.getItem("depositos"),window.localStorage.getItem("retiros")],
@@ -108,3 +168,38 @@ const config = {
     data: data,
     options: {}
 };
+	
+	        const nTrans = new Chart(
+            document.getElementById('nTrans'),
+            config
+        );
+}
+
+function loadPorc(){ //carga tabla de % de gastos
+const labels = [
+            'Depositos',
+            'Retiros',
+        ];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: '% de transacciones',
+        backgroundColor: 'rgb(255, 255, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [window.localStorage.getItem("totalIngresos"),window.localStorage.getItem("totalRetiros")],
+
+            }]
+};
+
+const config = {
+    type: 'pie',
+    data: data,
+    options: {}
+};
+	
+	        const nTrans = new Chart(
+            document.getElementById('porcentajes'),
+            config
+        );
+}
